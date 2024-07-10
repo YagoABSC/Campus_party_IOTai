a1811582247e5e6c9be4975880a19953025f4980
Yago <yagoabscosta@gmail.com>
2024-07-09 21:13:45 -0300

    Usando fakedata para simular variações e implementação interna da API

--- static_web/script.js
+++ static_web/script.js
@@ -1,14 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const temperatureElement = document.getElementById('temperature');
-    const humidityElement = document.getElementById('humidity');
+    const temperatureElement = document.getElementById('curTemp');
+    const humidityElement = document.getElementById('curHum');
   
     function fetchData() {
-      //fetch('http://esp32.local/data')
-      fetch('http://localhost/projetomakers/php/fake_data.php')
+      // fetch('http://esp32.local/data')
+      fetch('http://localhost/Campus_party_IOTai/php/fake_data.php')
         .then(response => response.json())
         .then(data => {
-          temperatureElement.textContent = data.temperature;
-          humidityElement.textContent = data.humidity;
+
+
+        temperatureElement.innerHTML = `<div>${data.temperature.toFixed(0)}°C</div>`;
+
+        humidityElement.innerHTML = `<i class="icone2 fas fa-tint" style="color: rgba(85, 158, 254, 1);"></i> ${data.humidity.toFixed(0)}%`;
+
+        if (temperatureElement <= 10) {
+
+            document.getElementById('temp-status').innerText = "Temperatura abaixo do ideal";
+
+        }else if(temperatureElement >= 26 && temperatureElement <= 50){
+
+            document.getElementById('temp-status').innerText = "Aviso: Temperatura acima do ideal";
+
+         }else if(temperatureElement > 50){
+            
+            document.getElementById('temp-status').innerText = "Perigo: Temperatura muito acima do normal!";
+
+        }else if(temperatureElement > 10 && temperatureElement < 26){
+        
+            document.getElementById('temp-status').innerText = "Temperatura ideal";
+        
+        }
+
+        if (humidityElement <= 30) {
+
+            document.getElementById('humi-status').innerText = "Umidade abaixo do ideal"
+        
+        } else if (humidityElement >= 60){
+           
+            document.getElementById('humi-status').innerText = "Umidade muito alta"
+        
+        } else{
+         
+            document.getElementById('humi-status').innerText = "Umidade ideal"
+       
+        }
+
         })
         .catch(error => console.error('Erro ao buscar dados:', error));
     }
